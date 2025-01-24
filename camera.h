631bd66e7c79a10bf32ef051e7f356505c8f852e
PetruCristian <naniacristian@yahoo.com>
2025-01-24 19:50:41 +0200

    Added limit to number of ray bounces in the scene

--- camera.h
+++ camera.h
@@ -5,9 +5,10 @@
 
 class camera {
 public:
-    double aspect_ratio = 1.0;
-    int image_height = 100;
-    int samples_per_pixel = 10;
+    double aspect_ratio = 1.0;      // Ratio of the image width to the image height
+    int image_height = 100;         // Image height
+    int samples_per_pixel = 10;     // Count of random samples per each pixel
+    int max_depth = 10;             // Maximum number of ray bounces
 
     void render(const hittable& world) {
         initialize();
@@ -22,7 +23,7 @@ public:
 
                 for (int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
 
                 write_color(std::cout, pixel_samples_scale * pixel_color);
@@ -90,12 +91,15 @@ private:
         // return radius * random_in_unit_disk();
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if (depth <= 0)
+            return color(0, 0, 0);
+
         hit_record rec;
 
         if (world.hit(r, interval(0, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
